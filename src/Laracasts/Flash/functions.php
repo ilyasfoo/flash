06ddcf9ed1be62c5a91dc82ec98b525085906b1e
Ilyas Foo <foo.ilyas@gmail.com>
2017-02-26 18:35:01 +0800

    Update functions.php

--- src/Laracasts/Flash/functions.php
+++ src/Laracasts/Flash/functions.php
@@ -9,12 +9,12 @@ if ( ! function_exists('flash')) {
      * @param  string      $level
      * @return \Laracasts\Flash\FlashNotifier
      */
-    function flash($message = null, $level = 'info')
+    function flash($message = null, $level = 'info', $namespace = 'flash')
     {
         $notifier = app('flash');
 
         if ( ! is_null($message)) {
-            return $notifier->message($message, $level);
+            return $notifier->message($message, $level, $namespace);
         }
 
         return $notifier;
